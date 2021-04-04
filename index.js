3705ec7d3fd9e51da8592f8e95c97182d259b57c
Veronika <veroniqe@rambler.ru>
2021-04-04 10:34:34 +0200

    fix

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ let request = {
   isResults: true,
 };
 
+function createAndAppend(name, parent) {
+  const elem = document.createElement(name);
+  parent.appendChild(elem);
+  return elem;
+}
+
 async function fetchData(url) {
   try {
-    console.log('we are in fetchData');
     const fetchedData = await fetch(url);
 
     if (fetchedData.ok) {
       const parsedData = await fetchedData.json();
-      console.log('parsedData:');
-      console.log(parsedData);
       if (!parsedData.meals) {
-        console.log('no food');
         request.isResults = false; //We change isResults
-        console.log('We changed request.isResult.  request:');
-        console.log(request);
-
         throw new Error(`Try to find something else!`);
       }
       request.isResults = true; //We change isResults
-      console.log('We changed request.isResult.  request:');
-      console.log(request);
-      showResults(parsedData);
+      return parsedData;
     } else {
       throw new Error(`Request to ${url} failed`);
     }
@@ -33,59 +30,48 @@ async function fetchData(url) {
 }
 
 function showResults(data) {
-  console.log('we are in showResults');
   const errDiv = document.querySelector('#error');
   errDiv.textContent = '';
   const showDiv = document.getElementById('dish-list');
   showDiv.textContent = '';
-  const ul = document.createElement('ul');
-
+  const ul = createAndAppend('ul', showDiv);
   data.meals.forEach((element) => {
-    const li = document.createElement('li');
+    const li = createAndAppend('li', ul);
     li.textContent = element.strMeal;
     li.className = 'link';
-    li.onclick = () => {
+    li.addEventListener('click', () => {
       showOneMeal(element);
-    };
-    ul.appendChild(li);
+    });
   });
-
-  showDiv.appendChild(ul);
 }
 
 function showOneMeal(meal) {
-  console.log('we are in showOneMeal');
   const showDiv = document.getElementById('dish-list');
   showDiv.textContent = '';
   const searchField = document.getElementById('searchField');
   searchField.value = '';
 
-  const h2 = document.createElement('h2');
+  const h2 = createAndAppend('h2', showDiv);
   h2.textContent = meal.strMeal;
-  showDiv.appendChild(h2);
 
-  const img = document.createElement('img');
+  const img = createAndAppend('img', showDiv);
   img.src = meal.strMealThumb;
   img.alt = meal.strMeal;
-  showDiv.appendChild(img);
 
-  const description = document.createElement('div');
+  const description = createAndAppend('div', showDiv);
   description.id = 'description';
-  showDiv.appendChild(description);
 
-  listOfIngredients(meal, description);
+  getListOfIngredients(meal, description);
 
-  const h3 = document.createElement('h3');
+  const h3 = createAndAppend('h3', description);
   h3.textContent = 'Instruction';
-  description.appendChild(h3);
 
-  const p = document.createElement('p');
+  const p = createAndAppend('p', description);
   p.textContent = meal.strInstructions;
-  description.appendChild(p);
 }
 
-function listOfIngredients(meal, description) {
-  const listOfIngredients = document.createElement('ul');
+function getListOfIngredients(meal, description) {
+  const listOfIngredients = createAndAppend('ul', description);
   listOfIngredients.id = 'listOfIngredients';
 
   const mealArr = Object.entries(meal);
@@ -103,58 +89,50 @@ function listOfIngredients(meal, description) {
     }
   }
   for (let i = 0; i < ingredients.length; i++) {
-    const li = document.createElement('li');
+    const li = createAndAppend('li', listOfIngredients);
     li.textContent = `${ingredients[i]}: ${measure[i]}`;
-
-    listOfIngredients.appendChild(li);
   }
-  description.appendChild(listOfIngredients);
 }
 
 function renderError(error) {
-  console.log('we are in renderError');
   const errorDiv = document.getElementById('error');
   errorDiv.textContent = '';
-  const errorP = document.createElement('p');
-  errorP.textContent = error;
-  errorDiv.appendChild(errorP);
   const showDiv = document.getElementById('dish-list');
   showDiv.textContent = '';
+  const errorP = createAndAppend('p', errorDiv);
+  errorP.textContent = error;
 }
 
-function main() {
-  console.log('we are in main()');
+async function main() {
   const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
   const searchField = document.getElementById('searchField');
   let token = 0;
 
-  searchField.oninput = () => {
-    console.log('some key is pressed');
+  searchField.addEventListener('input', () => {
     clearTimeout(token);
-    console.log('token is cleared');
     if (searchField.value.trim().length < 2) {
-      console.log('less then 2 letters');
       return;
     }
-    // if user got no results (we don't have the meal with this name) and
-    //he is continuing to add some letters in the input field we stop this function
+
     if (
       searchField.value.trim().includes(request.value) &&
       request.isResults === false
     ) {
-      console.log('request:');
-      console.log(request);
-      console.log('you continuing to add letters to unworking result');
       return;
     }
 
-    token = setTimeout(() => {
-      request.value = searchField.value; //we change request.value
-      console.log('We changed request vaue. request:');
-      console.log(request);
-      fetchData(url + searchField.value);
+    token = setTimeout(async () => {
+      request.value = searchField.value;
+      try {
+        const parsedData = await fetchData(url + searchField.value);
+        if (parsedData) {
+          showResults(parsedData);
+        }
+      } catch (error) {
+        renderError(error);
+      }
     }, 300);
-  };
+  });
 }
 
 window.addEventListener('load', main);
